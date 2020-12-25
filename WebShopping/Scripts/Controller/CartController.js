72d2a6346c5d7a8e40b72ffa4f634469cb223bc7
nguyenhuy-iot <nguyenhuy.iot@gmail.com>
2020-12-25 19:17:16 +0700

    dieu chinh gio hang

---
+++ WebShopping/Scripts/Controller/CartController.js
@@ -0,0 +1,23 @@
+﻿var cart = {
+    init: function () {
+        cart.regEvents();
+    },
+    regEvents: function () {
+        $('#btnContinue').off('click').on('click', function () {
+            window.location.href = "/Products";
+        });
+        $('#btnPayment').off('click').on('click', function () {
+            window.location.href = "/thanh-toan";
+        });
+        $('#btnUpdate').off('click').on('click', function () {
+           
+
+            
+        });
+
+        
+
+        
+    }
+}
+cart.init();
