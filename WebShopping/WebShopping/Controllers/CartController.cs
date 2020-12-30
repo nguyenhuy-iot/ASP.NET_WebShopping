6325c31fd7377cc325e8d3633cbb1e3f71e4c8ca
nguyenhuy-iot <nguyenhuy.iot@gmail.com>
2020-12-30 20:07:51 +0700

    tu dong them customer khi checkout

--- WebShopping/WebShopping/Controllers/CartController.cs
+++ WebShopping/WebShopping/Controllers/CartController.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Script.Serialization;
@@ -122,43 +123,75 @@ namespace WebShopping.Controllers
             ViewBag.infoCart = _item;
             return PartialView("BagCart");
         }
-        //[Authorize]
+        [Authorize]
         public ActionResult Checkout()
-        {            
-            string UserID = User.Identity.GetUserId();
-            string UserName = User.Identity.GetUserName();
-            Order OrderItem = new Order();
+        {
+            //string UserID = User.Identity.GetUserId();
+            //string UserName = User.Identity.GetUserName();  
+            ViewBag.UserName = User.Identity.GetUserName();
             return View();
         }
         [HttpPost]
         public ActionResult Checkout(FormCollection form)
         {
-            //Create Order "ID,CustomerID,OrderDate,RequreDate,ShipAddress,Phone"
-            Order OrderItem = new Order();
-            OrderItem.CustomerID = 1;
-            OrderItem.OrderDate = DateTime.Now;
-            OrderItem.RequreDate = DateTime.Parse(form["Requre_Date"]);
-            OrderItem.ShipAddress = form["Ship_Address"];
-            OrderItem.Phone = form["Phone"];
-            db.Order.Add(OrderItem);
-            //db.SaveChanges();
-            //Create OrderDetail "OrderID,ProductID,Quantity,UnitPrice"
-            var cart = Session[CartSession];
-            var list = (List<CartItem>)cart;
-            foreach (var item in list)
+            try
             {
-                OrderDetail orderDetail = new OrderDetail();
-                orderDetail.OrderID = OrderItem.ID;
-                orderDetail.ProductID = item.Product.ProductID;
-                orderDetail.Quantity = item.Quantity;
-                orderDetail.UnitPrice = item.Product.UnitPrice;
-                db.OrderDetail.Add(orderDetail);
-            }
-            db.SaveChanges();
-            //Delete All Cart
-            list.Clear();
+                //check Customer                
+                string Email = User.Identity.GetUserName();
+                int Customer_ID = 0;
+                var Cus = from s in db.Customer
+                               select s;
+                var C_user = Cus.Where(s => s.Email == Email).ToList();            
+                if (C_user.Count()>0)
+                {
+                    Customer_ID = C_user[0].ID;
 
-            return View("Checkout_Success");
+                }
+                else
+                {
+                    //Create Customer "ID,Name,Address,Phone,Email"
+                    Customer customer = new Customer();
+                    customer.Name = Email;
+                    customer.Address = form["Ship_Address"];
+                    customer.Phone = form["Phone"];
+                    customer.Email = Email;
+                    db.Customer.Add(customer);
+                    Customer_ID = customer.ID;
+                    //db.SaveChanges();
+                }
+
+                //Create Order "ID,CustomerID,OrderDate,RequreDate,ShipAddress,Phone"
+                Order OrderItem = new Order();
+                OrderItem.CustomerID = Customer_ID;
+                OrderItem.OrderDate = DateTime.Now;
+                OrderItem.RequreDate = DateTime.Parse(form["Requre_Date"]);
+                OrderItem.ShipAddress = form["Ship_Address"];
+                OrderItem.Phone = form["Phone"];
+                db.Order.Add(OrderItem);
+                //db.SaveChanges();
+                //Create OrderDetail "OrderID,ProductID,Quantity,UnitPrice"
+                var cart = Session[CartSession];
+                var list = (List<CartItem>)cart;
+                foreach (var item in list)
+                {
+                    OrderDetail orderDetail = new OrderDetail();
+                    orderDetail.OrderID = OrderItem.ID;
+                    orderDetail.ProductID = item.Product.ProductID;
+                    orderDetail.Quantity = item.Quantity;
+                    orderDetail.UnitPrice = item.Product.UnitPrice;
+                    db.OrderDetail.Add(orderDetail);
+                }
+                db.SaveChanges();
+                //Delete All Cart
+                list.Clear();
+                return View("Checkout_Success");
+            }
+            catch
+            {
+                return Content("Error Checkout. Kiểm tra lại thông tin");
+                
+            }
+            
         }
     }
 }
